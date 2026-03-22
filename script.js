43435a34e51e259f3b74fa523aa145469868ee9f
adrixprogra <adrixprogra27@gmail.com>
2026-03-21 20:31:08 -0600

    Remove redundant code for music playback

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 console.log("Fanpage de jime cargada <3");
-
-const music = document.getElementById("bg-music");
-
-window.addEventListener("load", () => {
-    music.play().catch(() => {
-        document.addEventListener("click", () => {
-            music.play();
-        }, { once: true });
-    });
-});
